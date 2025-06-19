bfdb8f0c6cc42392ee7a3a3d83901bf1db7e20e4
BitedRadish <a980831@naver.com>
2025-06-19 16:27:38 +0900

    feat : 수정

--- src/main/java/com/employee/deploy/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/employee/deploy/exception/advice/DefaultExceptionAdvice.java
@@ -23,18 +23,6 @@ import java.util.Map;
 @RestControllerAdvice
 @Slf4j
 public class DefaultExceptionAdvice {
-
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    public ResponseEntity<ErrorObject> handleResourceNotFoundException(ResourceNotFoundException ex) {
-//        ErrorObject errorObject = new ErrorObject();
-//        errorObject.setStatusCode(ex.getHttpStatus().value());
-//        errorObject.setMessage(ex.getMessage());
-//
-//        log.error(ex.getMessage(), ex);
-//
-//        return new ResponseEntity<ErrorObject>(errorObject, HttpStatusCode.valueOf(ex.getHttpStatus().value()));
-//    }
-
     /*
         Spring6 버전에 추가된 ProblemDetail 객체에 에러정보를 담아서 리턴하는 방법
      */
